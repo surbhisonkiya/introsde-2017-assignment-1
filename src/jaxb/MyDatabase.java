f426ef12372272354bb3e3ea7d0a9368df12941b
surbhisonkiya <31562927+surbhisonkiya@users.noreply.github.com>
2017-10-25 18:05:04 +0200

    Update MyDatabase.java

--- src/jaxb/MyDatabase.java
+++ src/jaxb/MyDatabase.java
@@ -1,17 +1,17 @@
 package jaxb;
 
-
 import java.util.Random;
-
 import xmlProfile.ActivityProfile;
 import xmlProfile.Person;
 
-
 public class MyDatabase {
+	//declaring database for person
 	private static String fNames[] = new String[] {"Rohit","Surbhi", "Pooja","Parul","HariOm"};
 	private static String lNames[] = new String[] {"Prakash","Sonkiya", "Pandey","Tank","Sharma"};
 	private static String bDates[] = new String[] {"13-06-1992","26-09-1991","14-03-1994","14-08-1987", "17-11-2000"};
+	//to generate id's for the activity preference of the person randomly
 	static Random rnd = new Random();
+	//declaring database for person's activity preference
 	static ActivityProfile tableTennis = new ActivityProfile(rnd.nextInt(100),"Table Tennis", "Smash the ball on the table", "Stuttgart", "13-10-2017");
 	static ActivityProfile yoga = new ActivityProfile(rnd.nextInt(100),"Yoga", "Yoga for health", "Trento", "11-09-2016");
 	static ActivityProfile chess = new ActivityProfile(rnd.nextInt(100),"Chess", "Play the mind of other person", "Bangalore", "18-08-2017");
@@ -21,11 +21,7 @@ public class MyDatabase {
 	private static ActivityProfile pActivities[] = new ActivityProfile[] {tableTennis,yoga,chess,running,badminton};
 	
 	public static void initializeDB(PersonActivityAnnotation people) {
-		Random random = new Random();
-		int max = 5;
-
 		for (int i = 0; i<5;i++) {
-			//Person person = new Person((long) i+1,fNames[random.nextInt(max)], lNames[random.nextInt(max)], bDates[random.nextInt(max)], pActivities[random.nextInt(max)]);
 			Person person = new Person((long) i+1,fNames[i], lNames[i], bDates[i], pActivities[i]);
 			people.getpersonData().add(person);
 		}
