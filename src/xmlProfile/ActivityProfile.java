5def77d78c039f53e66300172224e5288b7a1cea
surbhisonkiya <31562927+surbhisonkiya@users.noreply.github.com>
2017-10-25 20:26:36 +0200

    Update ActivityProfile.java

--- src/xmlProfile/ActivityProfile.java
+++ src/xmlProfile/ActivityProfile.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(propOrder = { "name", "description", "place", "startDate" })
 
 public class ActivityProfile {
-	@XmlAttribute(name="id") //adding new
+	@XmlAttribute(name="id") 
 	private int id;
 	private String name;
 	private String description;
@@ -36,7 +36,7 @@ public class ActivityProfile {
 		this.place = place;
 		this.startDate = startDate;
 	}
-	
+	// getters and setters for variables
 	public long getId() {
 		return id;
 	}
@@ -77,14 +77,15 @@ public class ActivityProfile {
 		this.startDate = startDate;
 	}
 	private String getRandomDate() {
-		int currentYear = Calendar.getInstance().get(Calendar.YEAR); 		// 1. get the current year
-		int year = (int) Math.round(Math.random()*(currentYear-1950)+1950); // 2. generate a random year 
+		//to generate 1. random year and 2.month.
+		int currentYear = Calendar.getInstance().get(Calendar.YEAR); 		
+		int year = (int) Math.round(Math.random()*(currentYear-1950)+1950); 
 																			//    between 1950 and currentYear
-		int month = (int) Math.round(Math.floor(Math.random()*11)+1);		// 3. select a random month of the year
-		// 4. select a random day in the selected month
-		// 4.1 prepare a months array to store how many days in each month
+		int month = (int) Math.round(Math.floor(Math.random()*11)+1);		
+		// 3. select a random day in the selected month
+		// 3.1 prepare an array to store how many days in each month
 		int[] months = new int[]{31,28,30,30,31,30,31,31,30,31,30,31};
-		// 4.2 if it is a leap year, feb (months[1]) should be 29
+		// 3.2 if it is a leap year, feb (months[1]) should be 29
 		if ((currentYear % 4 == 0) && ((currentYear % 100 != 0) || (currentYear % 400 == 0))) {
 			months[1] = 29;
 		}
@@ -92,7 +93,7 @@ public class ActivityProfile {
 		return ""+year+"-"+month+"-"+day;
 	}
 
-	// Modify to String to add the missing attributes.
+	// Override toString.
 	public String toString() {
 		return "Name="+name+", Description="+description + "Place="+place+"StartDate="+startDate;
 	}
