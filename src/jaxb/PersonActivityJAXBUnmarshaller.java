abe2028ad5f1c57e23f158dcb60a94a74780e5c8
surbhisonkiya <31562927+surbhisonkiya@users.noreply.github.com>
2017-10-25 18:21:26 +0200

    Update PersonActivityJAXBUnmarshaller.java

--- src/jaxb/PersonActivityJAXBUnmarshaller.java
+++ src/jaxb/PersonActivityJAXBUnmarshaller.java
@@ -23,13 +23,13 @@ public class PersonActivityJAXBUnmarshaller {
 
 	public void unMarshall(File xmlDocument) {
 		try {
-
+			//creating JAXBContext with an instance using the auto generated package
 			JAXBContext jaxbContext = JAXBContext.newInstance("generatedClass");
-
+			
 			Unmarshaller unMarshaller = jaxbContext.createUnmarshaller();
 			SchemaFactory schemaFactory = SchemaFactory
 					.newInstance("http://www.w3.org/2001/XMLSchema");
-			Schema schema = schemaFactory.newSchema(new File("people.xsd"));
+			Schema schema = schemaFactory.newSchema(new File("people.xsd")); // "people.xsd" is the XML schema
 			unMarshaller.setSchema(schema);
 			CustomValidationEventHandler validationEventHandler = new CustomValidationEventHandler();
 			unMarshaller.setEventHandler(validationEventHandler);
@@ -38,7 +38,7 @@ public class PersonActivityJAXBUnmarshaller {
 			People people = (People) unMarshaller
 					.unmarshal(xmlDocument);
 			List<generatedClass.People.Person> personList = (List<Person>) people.getPerson();
-
+			//print the output of the unmarshall in the console in below format
 			for (int i=0; i < personList.size();i++) {
 				int j = i+1;
 				System.out.println("Person: "+j+" ");
@@ -59,10 +59,9 @@ public class PersonActivityJAXBUnmarshaller {
 	}
 
 	public static void main(String[] argv) {
-		File xmlDocument = new File("people.xml");
-		
+		//input file for unmarshalling is "people.xml"
+		File xmlDocument = new File("people.xml");		
 		PersonActivityJAXBUnmarshaller jaxbUnmarshaller = new PersonActivityJAXBUnmarshaller();
-
 		jaxbUnmarshaller.unMarshall(xmlDocument);
 
 	}
