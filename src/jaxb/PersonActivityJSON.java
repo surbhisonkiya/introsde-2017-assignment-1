ddc20f59e2542e17e65f85d56078557600400e4b
surbhisonkiya <31562927+surbhisonkiya@users.noreply.github.com>
2017-10-25 19:18:22 +0200

    Update PersonActivityJSON.java

--- src/jaxb/PersonActivityJSON.java
+++ src/jaxb/PersonActivityJSON.java
@@ -15,19 +15,21 @@ public class PersonActivityJSON {
 		
 		MyDatabase.initializeDB(people);
 		
-		// Jackson Object Mapper 
+		// JSON Object Mapper 
 		ObjectMapper mapper = new ObjectMapper();
 		
-		// Adding the Jackson Module to process JAXB annotations
+		// Add the JSON Module to process JAXB annotations
         JaxbAnnotationModule module = new JaxbAnnotationModule();
         
-		// configure as necessary
+		// mapper configurations
 		mapper.registerModule(module);
 		mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
         mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
-
+	//getting the output in the string variable
         String result = mapper.writeValueAsString(people);
+	//print the output in the console
         System.out.println(result);
+	//print the output in the "people.json" file
         mapper.writeValue(new File("people.json"), people);
     }
 }
